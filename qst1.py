82c1b308da73b0427ee0d015bcbbb4d7defe646a
Kaiocesar67 <kaiocesarm2017@gmail.com>
2026-03-23 15:43:00 -0300

    Add files via upload

---
+++ qst1.py
@@ -0,0 +1,2 @@
+meses = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+print(meses[0], meses[1], meses[-1])
---
+++ qst3.py
@@ -0,0 +1,14 @@
+timesbrasileiraoserieA = ("Botafogo", "Palmeiras", "Flamengo", "Internacional", "Fortaleza", "São Paulo", "Corinthians", "Bahia", "Cruzeiro", "Vasco", "EC Vitória", "Grêmio", "Juventude", "Atlético-MG", "Fluminense", "Athletico-PR", "Bragantino", "Criciúma", "Atlético-GO", "Cuiabá")
+print(f"""Times classificados para a Libertadores
+      ==============================
+      {timesbrasileiraoserieA[0]}
+      {timesbrasileiraoserieA[1]}
+      {timesbrasileiraoserieA[2]}
+      {timesbrasileiraoserieA[3]}
+      ==============================
+      Times na zona de rebaixamento
+      ==============================
+      {timesbrasileiraoserieA[16]}
+      {timesbrasileiraoserieA[17]}
+      {timesbrasileiraoserieA[18]}
+      {timesbrasileiraoserieA[19]}""")
